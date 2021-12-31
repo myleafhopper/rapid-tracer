48e41efde5ca04f82f31836edcc90951d0eaa7f3
Prince Choudhury <myleafhopper@gmail.com>
2021-12-30 22:19:27 -0500

    feat: add support for all custom data types

--- server/services/data/Helper.js
+++ server/services/data/Helper.js
@@ -22,6 +22,8 @@ module.exports.getRows = (body) => {
 SETUP
 -------------------------------------------------- */
 
+const { v4: uuidv4 } = require('uuid');
+const creditCardManager = require('./CreditCardManager');
 const configuration = require('./configuration.json');
 const predefinedData = {};
 const dataNames = [];
@@ -109,22 +111,115 @@ const setCustomDataValue = (data, column) => {
     if (column.dataName === 'App Version') {
 
         data[column.dataName] = getRandomNumber(1, 9) + '.' +
-        getRandomNumber(1, 20) + '.' + getRandomNumber(1, 200);
+            getRandomNumber(1, 20) + '.' +
+            getRandomNumber(1, 200);
 
     } else if (column.dataName === 'Boolean') {
 
         data[column.dataName] = getRandomValueFromList([false, true]);
 
+    } else if (column.dataName === 'Characters') {
+
+        data[column.dataName] = getRandomString(column.modifierSetup);
+
+    } else if (column.dataName === 'Credit Card Number') {
+
+        const cardType = column.modifierSetup.creditCardType;
+        data['Credit Card Number'] = creditCardManager.generateCardNumber(cardType);
+        data['Credit Card Type'] = cardType;
+
+    } else if (column.dataName === 'Custom List') {
+
+        data[column.dataName] = getRandomValueFromList(column.modifierSetup.list);
+
+    } else if (column.dataName === 'Date (DD/MM/YYYY)') {
+
+        const date = getDateObject();
+        data[column.dataName] = `${date['DD']}/${date['MM']}/${date['YYYY']}`;
+
+    } else if (column.dataName === 'Date (MM/DD/YYYY)') {
+
+        const date = getDateObject();
+        data[column.dataName] = `${date['MM']}/${date['DD']}/${date['YYYY']}`;
+
+    } else if (column.dataName === 'Email') {
+
+        const lastName = getRandomValueFromList(predefinedData['last_names.json'].list);
+        const number = getRandomNumber(1111, 9999);
+        const emailProvider = getRandomValueFromList(['gmail', 'yahoo', 'hotmail']);
+        data[column.dataName] = `${lastName}${number}@${emailProvider}.com`;
+
+    } else if (column.dataName === 'Full Name (Female)') {
+
+        const firstName = getRandomValueFromList(predefinedData['female_first_names.json'].list);
+        const lastName = getRandomValueFromList(predefinedData['last_names.json'].list);
+        data[column.dataName] = `${firstName} ${lastName}`;
+
+    } else if (column.dataName === 'Full Name (Male)') {
+
+        const firstName = getRandomValueFromList(predefinedData['male_first_names.json'].list);
+        const lastName = getRandomValueFromList(predefinedData['last_names.json'].list);
+        data[column.dataName] = `${firstName} ${lastName}`;
+
+    } else if (column.dataName === 'GUID') {
+
+        data[column.dataName] = uuidv4();
+
+    } else if (column.dataName === 'Latitude') {
+
+        data[column.dataName] = getGeoCoordinate(-90, 90, 3);
+
+    } else if (column.dataName === 'Longitude') {
+
+        data[column.dataName] = getGeoCoordinate(-180, 180, 3);
+
     } else if (column.dataName === 'Number') {
 
         data[column.dataName] = getRandomNumber(
-            column.settings.minimum,
-            column.settings.maximum
+            column.properties.minimum,
+            column.properties.maximum
         );
 
-    } else if (column.dataName === 'Characters') {
+    } else if (column.dataName === 'Password') {
 
-        data[column.dataName] = getRandomString(column.settings);
+        data[column.dataName] = getRandomString(column.modifierSetup);
+
+    } else if (column.dataName === 'Phone Number (USA)') {
+
+        data[column.dataName] = getRandomNumber(111, 999) +
+            getRandomNumber(1111, 9999) +
+            getRandomNumber(1111, 9999);
+
+    } else if (column.dataName === 'Postal Code (USA)') {
+
+        data[column.dataName] = getRandomNumber(11111, 99999);
+
+    } else if (column.dataName === 'Social Security Number (USA)') {
+
+        data[column.dataName] = getRandomNumber(111, 999) +
+            getRandomNumber(11, 99) +
+            getRandomNumber(1111, 9999);
+
+    } else if (column.dataName === 'Time (HH:MM)') {
+
+        const date = getTimeObject();
+        data[column.dataName] = `${date['HH']}:${date['MM']}`;
+
+    } else if (column.dataName === 'Time (HH:MM:SS)') {
+
+        const date = getTimeObject();
+        data[column.dataName] = `${date['HH']}:${date['MM']}:${date['SS']}`;
+
+    } else if (column.dataName === 'Time (HH:MM:SS.SSS)') {
+
+        const date = getTimeObject();
+        data[column.dataName] = `${date['HH']}:${date['MM']}:${date['SS']}.${date['SSS']}`;
+
+    } else if (column.dataName === 'Username') {
+
+        const lastName = getRandomValueFromList(predefinedData['last_names.json'].list);
+        const number = getRandomNumber(1111, 9999);
+        data[column.dataName] = `${lastName}${number}`;
     }
 };
 
@@ -136,19 +231,19 @@ const getRandomNumber = (minimum, maximum) => {
     return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 };
 
-const getRandomString = (settings) => {
+const getRandomString = (modifierSetup) => {
 
     let characters = '';
 
-    if (settings.includeUpperCaseCharacters) {
+    if (modifierSetup.includeUpperCaseCharacters) {
         characters = characters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
 
-    if (settings.includeLowerCaseCharacters) {
+    if (modifierSetup.includeLowerCaseCharacters) {
         characters = characters + 'abcdefghijklmnopqrstuvwxyz';
     }
 
-    if (settings.includeNumber) {
+    if (modifierSetup.includeNumber) {
         characters = characters + '0123456789';
     }
 
@@ -156,7 +251,7 @@ const getRandomString = (settings) => {
     const maximum = characters.length;
     const minimum = 0;
 
-    for (let i = 0; i < settings.size; i++) {
+    for (let i = 0; i < modifierSetup.size; i++) {
         collection.push(
             characters.charAt(
                 Math.floor(Math.random() * (maximum - minimum)) + minimum
@@ -165,4 +260,31 @@ const getRandomString = (settings) => {
     }
 
     return collection.join('');
+};
+
+const getDateObject = () => {
+
+    const date = new Date();
+
+    return {
+        'DD': date.getDate(),
+        'MM': date.getMonth() + 1,
+        'YYYY': date.getFullYear()
+    };
+};
+
+const getGeoCoordinate = (start, end, decimalLength) => {
+    return (Math.random() * (end - start) + start).toFixed(decimalLength) * 1;
+};
+
+const getTimeObject = () => {
+
+    const date = new Date();
+
+    return {
+        'HH': date.getHours(),
+        'MM': date.getMinutes(),
+        'SS': date.getSeconds(),
+        'SSS': date.getMilliseconds()
+    };
 };
